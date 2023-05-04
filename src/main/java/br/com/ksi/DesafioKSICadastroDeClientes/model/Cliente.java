23ec0aee004edb888da1077f74f474ee1c0a7d3f
giovanesantiago <giovanedevweb@gmail.com>
2023-05-04 17:45:31 -0300

    Modelo de Cliente ok

---
+++ src/main/java/br/com/ksi/DesafioKSICadastroDeClientes/model/Cliente.java
@@ -0,0 +1,53 @@
+package br.com.ksi.DesafioKSICadastroDeClientes.model;
+
+import java.util.Date;
+
+public class Cliente {
+
+    private Long id;
+    private String nome;
+    private String cpf;
+    private Date dataNascimento;
+
+    public Cliente(Long id, String nome, String cpf, Date dataNascimento) {
+        this.id = id;
+        this.nome = nome;
+        this.cpf = cpf;
+        this.dataNascimento = dataNascimento;
+    }
+
+    public Cliente() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public Date getDataNascimento() {
+        return dataNascimento;
+    }
+
+    public void setDataNascimento(Date dataNascimento) {
+        this.dataNascimento = dataNascimento;
+    }
+}
