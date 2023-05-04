01458b3752c789be9ec08ce2d5952f049d90dffb
giovanesantiago <giovanedevweb@gmail.com>
2023-05-04 17:37:12 -0300

    Iniciando projeto

---
+++ src/main/java/br/com/ksi/DesafioKSICadastroDeClientes/DesafioKsiCadastroDeClientesApplication.java
@@ -0,0 +1,13 @@
+package br.com.ksi.DesafioKSICadastroDeClientes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesafioKsiCadastroDeClientesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DesafioKsiCadastroDeClientesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/ksi/DesafioKSICadastroDeClientes/DesafioKsiCadastroDeClientesApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.ksi.DesafioKSICadastroDeClientes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesafioKsiCadastroDeClientesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
