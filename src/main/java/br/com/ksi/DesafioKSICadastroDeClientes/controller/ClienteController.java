e0cc11d6dff5beeb3453457d131a1613222455fd
giovanesantiago <giovanedevweb@gmail.com>
2023-05-04 19:54:03 -0300

    iniciando listagem de cliente no painel

--- src/main/java/br/com/ksi/DesafioKSICadastroDeClientes/controller/ClienteController.java
+++ src/main/java/br/com/ksi/DesafioKSICadastroDeClientes/controller/ClienteController.java
@@ -4,19 +4,37 @@ import br.com.ksi.DesafioKSICadastroDeClientes.model.Cliente;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Controller
 public class ClienteController {
+
+    // Lista de clientes temporaria para testes antes de integrar Banco de dados
+    List<Cliente> clienteList = new ArrayList<>();
 
     // Rota para Abrir tela de cadastro
     @GetMapping("/create")
     public String home(){
         return "create";
     }
+    // Rota para Abrir painel de clientes
+    @GetMapping("/painel")
+    public ModelAndView painel() {
+        ModelAndView mv = new ModelAndView("painel");
+        mv.addObject("clienteList", clienteList);
+        return mv;
+    }
 
+    // Metodo para cadastro de cliente
     @PostMapping("/create")
-    public void create(Cliente cliente) {
-        System.out.println("O nome da cliente é : " + cliente.getNome()
-                + " Ele aceitou os termos : " + cliente.getTermosPoliticas());
+    public String create(Cliente cliente) {
+        Long id = clienteList.size() + 1L;
+        cliente.setId(id);
+        clienteList.add(cliente);
+
+        return "confirmaçãoCadastro";
     }
 }
