dbd5eefd8d07cd1bee4b547035db51933a9fbe10
giovanesantiago <giovanedevweb@gmail.com>
2023-05-04 19:57:47 -0300

    alterando nome html para evitar bugs

--- src/main/java/br/com/ksi/DesafioKSICadastroDeClientes/controller/ClienteController.java
+++ src/main/java/br/com/ksi/DesafioKSICadastroDeClientes/controller/ClienteController.java
@@ -35,6 +35,6 @@ public class ClienteController {
         cliente.setId(id);
         clienteList.add(cliente);
 
-        return "confirmaçãoCadastro";
+        return "confirmacaoCadastro";
     }
 }
